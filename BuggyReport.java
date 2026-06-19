8c37f96eee47556a5f84cc83cff57bd34251175c
ashaec0101 <ashaec0101@gmail.com>
2026-06-19 14:52:28 -0700

    Fixed bug 4, added return statement in if statement. Note: Bug 5 was fixed first.

--- BuggyReport.java
+++ BuggyReport.java
@@ -59,7 +59,9 @@ public class BuggyReport {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
                 int found = i;
+                return found;
             }
+            //FIXED
         }
         return -1;
     }
